459307c50e24f2ced54b86162e188bcb844f5b1e
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-04-13 12:05:43 -0700

    Convert the Rollfile to a bunch of Clojure code for generating the old one

--- Rollfile.clj
+++ Rollfile.clj
@@ -1,162 +1,54 @@
-{;; Profiles are a mechanism for providing configuration both of targets and tasks.
- ;; Profiles are either maps of profile data, or a sequence containing either keywords naming other
- ;; profiles to apply or maps of profile data.
- :profiles
- {;; The default profile stack which Katamari attempts to apply.
-  ;; FIXME: How does profile de-activation work? Does it work? Do we need it?
-  :katamari/default
-  [:base :system :user :provided :dev]
+;; Rollfile.clj
+;;
+;; This file is a normal Clojure program which produces a katamari build configuration.
+;; The state it produces is represented in Rollfile.edn
 
-  ;; Base is the most fundamental profile, and will come from Katamari itself.
-  ;; There shouldn't really be a reason to manipulate it ever.
-  :base
-  {;; Options used by all Maven targets
-   :maven
-   {:repo "~/.m2"
-    :repositories [{:names ["central", "maven-central"]
-                    :urls ["https://repo1.maven.org/maven2"]
-                    :snapshots false}
-                   {:names ["clojars"]
-                    :urls ["https://repo.clojars.org"]}]}}
+(require '[katamari.core :as kat])
 
-  ;; System is a concept which exists in Leiningen and may or may not make sense.  The system
-  ;; profile may be defined in an /etc/katamari.edn file, to provide global options.  Consequently
-  ;; system profiles should be minimal, and are probably best used only for organizational
-  ;; configuration such as defining repositories.
-  :system
-  {}
+(defn contrib-style-clojure-library [build name options]
+  (kat/clojure-library build name
+     (-> options
+         (update-in :base :paths (fnil into [])
+                    [(str name "/src/main/clj")
+                     (str name "/src/main/cljc")
+                     (str name "/src/main/resources")])
+         (update-in :dev :paths (fnil into [])
+                    [(str name "/src/dev/clj")
+                     (str name "/src/dev/cljc")
+                     (str name "/src/dev/resources")])
+         (update-in :test :paths (fnil into [])
+                    [(str name "/src/test/clj")
+                     (str name "/src/test/cljc")
+                     (str name "/src/test/resources")]))))
 
-  ;; User may be defined in ~/.katamari/profiles.clj
-  ;; The user profile is intended for providing user-specific environment customization.
-  ;; For instance setting a prompt, or adding ones own toolkit.
-  :user
-  {}
+(-> (kat/empty-build)
+    (kat/load-profiles "/etc/katamari.edn")
+    (kat/load-profiles "~/.katamari/profiles.edn")
 
-  ;; Tasks, such as test, compile, uberjar, repl and soforth all execute with a special-purpose
-  ;; profile named for the task. User defined tasks likewise execute with an additional profile
-  ;; named for the task's alias.
-  :test
-  {}
+    ;; Make dependencies available as targets
+    (kat/mvn-dep '[org.clojure/clojure "1.9.0"])
+    (kat/mvn-dep '[org.clojure/specs.alpha "0.1.143"])
+    (kat/mvn-dep '[org.clojure/tools.deps.alpha "0.5.417"])
+    (kat/mvn-dep '[org.clojure/test.check "0.9.0"])
+    (kat/mvn-dep '[org.clojure/tools.nrepl "0.2.12"])
+    (kat/mvn-dep '[io.replikativ/hasch "0.3.4"])
 
-  :compile
-  {}
+    ;; Clojure libraries
+    (contrib-style-clojure-library katamari-core
+      {:dependencies [org.clojure/clojure
+                      org.clojure/specs.alpha
+                      org.clojure/tools.deps.alpha
+                      io.replikativ/hasch]})
 
-  :repl
-  {}
+    (contrib-style-clojure-library katamari-server
+      {:dependencies [katamari-core
+                      org.clojure/tools.nrepl]})
 
-  :uberjar
-  {}
+    ;; Roll everything up for easy deployment
+    (kat/mvn-artifact 'me.arrdem/katamari
+      '{:dependencies [katamari-server]
+        :version "0.0.0"})
 
-  :deploy
-  {}
-  }
+    ;; Lets do it!
+    (kat/roll!))
 
- :targets
- {;; Targets are things which Katamari "knows" how to build.
-  ;; They're named by a symbol, and defined by a pair `[target-type target-config]`.
-  ;; Targets become "configured" based on the profiles which Katamari is rolling with.
-  ;; Consequently target configuration is a map from profile names to configuration.
-  ;; Targets usually just specify the `:default` profile.
-
-  org.clojure/clojure
-  ;; This is a Maven target.
-  ;; It uses the `:maven` profile options together with whatever configuration is applied here.
-  ;; Maven artifacts are downloaded from a `:repository` which must name a known Maven repository
-  ;; at a given `:group`, `:artifact`, `:version`, `:qualifier` address.
-  ;;
-  ;; Maven targets implicitly depend on their own dependencies.
-  ;; Users are not required to manage those.
-  ;; When Maven targets are used as dependencies of other targets, dependency resolution occurs.
-  ;; That is, version conflicts are deferred until Katamari rolls a target which forces a conflict.
-  ;;
-  ;; This behavior is chosen to align with Leiningen and Twitter's Pants, as opposed to Facebook's
-  ;; Buck which requires that transitive dependencies be manually managed.
-  [:maven
-   {:base
-    {:repository "central"
-     :coordinates {:group "org.clojure"
-                   :artifact "clojure"
-                   :version "1.9.0"}}}]
-
-  org.clojure/specs.alpha
-  [:maven
-   {:base
-    {:repository  "central"
-     :coordinates {:group    "org.clojure"
-                   :artifact "specs.alpha"
-                   :version  "0.1.143"}}}]
-
-  org.clojure/tools.deps.alpha
-  [:maven
-   {:base
-    {:repository  "central"
-     :coordinates {:group    "org.clojure"
-                   :artifact "tools.deps.alpha"
-                   :version  "0.5.417"}}}]
-
-  org.clojure/test.check
-  [:maven
-   {:base
-    {:repository  "central"
-     :coordinates {:group    "org.clojure"
-                   :artifact "test.check"
-                   :version  "0.9.0"}}}]
-
-  org.clojure/tools.nrepl
-  [:maven
-   {:base
-    {:repository  "central"
-     :coordinates {:group    "org.clojure"
-                   :artifact "tools.nrepl"
-                   :version  "0.2.12"}}}]
-
-  io.replikativ/hasch
-  [:maven
-   {:base
-    {:repository  "clojars"
-     :coordinates {:group    "io.replikativ"
-                   :artifact "hasch"
-                   :version  "0.3.4"}}}]
-
-  katamari-core
-  ;; This is a Clojure target.  It has `:paths`, being directory trees to be added to the classpath,
-  ;; eg. for adding Clojure sources or Java resources and `:dependencies`, being a sequence of other
-  ;; targets which it depends on. Depended targets are checked and executed as needed in topological
-  ;; sort order when they change.
-  [:clojure
-   {:base
-    {:paths ["katamari-core/src/main/cljc"
-             "katamari-core/src/main/clj"
-             "katamari-core/src/main/resources"]
-     :dependencies [org.clojure/clojure
-                    org.clojure/specs.alpha
-                    org.clojure/tools.deps.alpha
-                    io.replikativ/hasch]}
-    ;; This target provides some additional configuration for development use
-    :dev
-    {:paths ["katamari-core/src/dev/cljc"
-             "katamari-core/src/dev/clj"
-             "katamari-core/src/dev/resources"]}
-    ;; And for testing
-    :test
-    {:paths ["katamari-core/src/test/cljc"
-             "katamari-core/src/test/clj"
-             "katamari-core/src/test/resources"]
-     :dependencies [org.clojure/test.check]}]}
-
-  katamari-server
-  [:clojure-library
-   {:base
-    {:paths ["katamari-server/src/main/cljc"
-             "katamari-server/src/main/clj"]
-     :dependencies [katamari-core
-                    org.clojure/tools.nrepl]}
-    :dev
-    {:paths ["katamari-core/src/dev/cljc"
-             "katamari-core/src/dev/clj"]}}]
-
-  katamari
-  [:clojure-library
-   {:base
-    {:dependencies [katamari-core
-                    katamari-server]}}]}}
