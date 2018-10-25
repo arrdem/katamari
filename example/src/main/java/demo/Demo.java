d88407a818ae776ddd7b4acd552baca70445b61e
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-10-25 00:25:24 -0700

    Add a tree of examples and a dev ns

---
+++ example/src/main/java/demo/Demo.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class Demo {
+    public static int main(String[] args) {
+        System.out.println(String.format("Got %d args!", args.length()));
+        return 0;
+    }
+}
