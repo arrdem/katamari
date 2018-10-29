3138794862677df81224c5e207894d6c300a3a8b
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-10-28 18:39:57 -0700

    Fix build issues :awesome:

--- example/src/main/java/demo/Demo.java
+++ example/src/main/java/demo/Demo.java
@@ -2,7 +2,7 @@ package demo;
 
 public class Demo {
     public static int main(String[] args) {
-        System.out.println(String.format("Got %d args!", args.length()));
+        System.out.println(String.format("Got %d args!", args.length));
         return 0;
     }
 }
