824bbd8daf0214158032a543639ff9ae00f57a08
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-11-04 19:33:50 -0800

    A more test-like bootstrap script

--- etc/bootstrap.sh
+++ etc/bootstrap.sh
@@ -4,9 +4,27 @@
 # tries to reboot into the new jar, and if that fails puts the old jar back.
 
 jar=$(./kat -j compile me.arrdem/katamari+uberjar | jq -r '.["me.arrdem/katamari+uberjar"]["paths"][0]')
-mv .kat.d/bootstrap.jar kat-backup.jar
-cp $jar .kat.d/bootstrap.jar
-./kat restart-server || (
-    cp kat-backup.jar .kat.d/bootstrap.jar
-    ./kat start-server
-)
+if [ -z "$jar" ]; then
+    exit 1
+else
+    mv .kat.d/bootstrap.jar kat-backup.jar
+    cp $jar .kat.d/bootstrap.jar
+
+    # Try standalone
+    KAT_SERVER_CP=.kat.d/bootstrap.jar ./kat restart-server && \
+        ./kat compile me.arrdem/katamari+uberjar || (
+        cp kat-backup.jar .kat.d/bootstrap.jar
+        ./kat start-server
+    )
+
+    echo "Standalone seemingly OK..."
+
+    # Try in "normal" configuration with the sources paths
+    ./kat restart-server && \
+        ./kat compile me.arrdem/katamari+uberjar || (
+        cp kat-backup.jar .kat.d/bootstrap.jar
+        ./kat start-server
+    )
+
+    echo "Still looking good!"
+fi
