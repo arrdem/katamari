2bd5ff1ee69f0f861f011b872c2ce30fa40339e2
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-11-05 00:38:05 -0800

    Initialize & canonicalize deps at prep time
    This patch leverages the manifest prep time across all tools.deps (eg. JVM)
    backed targets to run some shared initializer code in the JVM extension that
    reads Katamari's current global default deps.edn data sources.
    
    After that step in manifest prep, at rule prep time all the JVM manifests use
    the version pinning information currently somewhat magically delivered by the
    deps config to rewrite! their dependencies. This fixes #19 properly, by making
    the pinned and versioned deps part of the rule as it is content hashed for
    fingerprinting.
    
    A (global) version bump will now cause target(s) to become stale.
    
    Fixes #16, #19

--- src/roll-jvm/src/roll/extensions/clj.clj
+++ src/roll-jvm/src/roll/extensions/clj.clj
@@ -3,7 +3,8 @@
   {:authors ["Reid 'arrdem' McKenzie <me@arrdem.com>"]}
   (:require [clojure.spec.alpha :as s]
             [roll.specs :as rs]
-            [roll.extensions :as ext]))
+            [roll.extensions :as ext]
+            [roll.extensions.jvm :as rejvm]))
 
 ;;;; Clojure library
 
@@ -14,6 +15,12 @@
 (ext/defmanifest clojure-library
   (s/keys* :opt-un [::rs/deps
                     ::rs/paths]))
+
+(defmethod ext/manifest-prep 'clojure-library [config buildgraph _manifest]
+  (rejvm/init-deps config buildgraph))
+
+(defmethod ext/rule-prep 'clojure-library [config buildgraph target rule]
+  (rejvm/canonicalize-deps config buildgraph target rule))
 
 (defmethod ext/rule-inputs 'clojure-library
   [config {:keys [targets] :as buildgraph} target rule]
@@ -26,7 +33,6 @@
 (defmethod ext/rule-build 'clojure-library
   [config buildgraph target rule products inputs]
   (merge
-   {:type ::product
-    :from target
+   {:from target
     :mvn/manifest :roll}
    (select-keys rule [:paths :deps])))
